f47a0421a62b5bc5d1a2bff44df50e755e554cfe
gness1804 <graham.nessler@gmail.com>
2020-11-02 20:43:23 -0600

    set uses on button click

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,11 +1,23 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 function Users () {
   const [users, setUsers] = useState([]);
 
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+      setUsers(data);
+    } catch (error) {
+      throw new Error('Failed to fetch users!');
+    }
+  }
+
   return (
     <div>
       {!users.length && <p>No users yet. Please click the button to see them.</p>}
+
+      <button onClick={getUsers}>See Users</button>
     </div>
   )
 }
