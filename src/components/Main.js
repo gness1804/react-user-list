41d970c7d7b0f434af0576a713fa158b3a9f185c
gness1804 <graham.nessler@gmail.com>
2020-11-02 20:18:07 -0600

    Start building out main

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './Main.css';
+
+function Main(){
+  return (
+    <div className="main">
+      <h2>See below for our current users:</h2>
+    </div>
+  );
+}
+
+export default Main;
